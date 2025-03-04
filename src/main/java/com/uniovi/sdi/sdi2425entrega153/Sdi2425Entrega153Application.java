b6fd3a343b44c834b4cda4b4280c8c25feddd0b2
uo270457 <uo270457@uniovi.es>
2025-03-04 09:32:11 +0100

    Inicio

---
+++ src/main/java/com/uniovi/sdi/sdi2425entrega153/Sdi2425Entrega153Application.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2425entrega153;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2425Entrega153Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2425Entrega153Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniovi/sdi/sdi2425entrega153/Sdi2425Entrega153ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2425entrega153;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2425Entrega153ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
